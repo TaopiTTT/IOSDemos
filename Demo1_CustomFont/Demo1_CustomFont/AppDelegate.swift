8c54fd001bcc5203e1e257f40f73229a060287a1
TaopiTTT <taopittt@qq.com>
2025-11-02 20:42:07 +0800

    完成VIewcontroller编程

--- Demo1_CustomFont/Demo1_CustomFont/AppDelegate.swift
+++ Demo1_CustomFont/Demo1_CustomFont/AppDelegate.swift
@@ -10,9 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow.init()
+        window?.makeKeyAndVisible()
+        let View = ViewController()
+        let navigationController = UINavigationController(rootViewController: View)
+        window?.rootViewController = navigationController
         // Override point for customization after application launch.
         return true
     }
