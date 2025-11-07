69c4e3a23f846d19310e5daacec6fcd709db8000
TaopiTTT <taopittt@qq.com>
2025-11-07 21:12:22 +0800

    initDemo2

---
+++ Demo2_StopWatch/Demo2_StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo2_StopWatch
+//
+//  Created by Tao on 2025/11/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
