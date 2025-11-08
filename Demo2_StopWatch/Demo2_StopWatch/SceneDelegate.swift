07b5b3f8aa2ddb126d2b760154ac3f05bffac2d6
TaopiTTT <taopittt@qq.com>
2025-11-08 15:34:56 +0800

    create All Buttons

--- Demo2_StopWatch/Demo2_StopWatch/SceneDelegate.swift
+++ Demo2_StopWatch/Demo2_StopWatch/SceneDelegate.swift
@@ -10,7 +10,14 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow.init()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        // Override point for customization after application launch.
+        return true
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
--- Demo2_StopWatch/Demo2_StopWatch/ViewController.swift
+++ Demo2_StopWatch/Demo2_StopWatch/ViewController.swift
@@ -7,11 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let ScreenWidth = UIScreen.main.bounds.width
+let ScreenHeight = UIScreen.main.bounds.height
 
+class ViewController: UIViewController {
+    
+    
+    
+    lazy var resetButton:UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.frame = CGRect(x: ScreenWidth*0.75, y: 40, width: 80, height: 60)
+        button.backgroundColor = .orange
+        button.layer.cornerRadius = 20
+        button.setTitle("Reset", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(tapResetButton), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var startButton:UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.frame = CGRect(x: 0, y: ScreenHeight*0.7, width: ScreenWidth*0.5, height: ScreenHeight*0.3)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 20
+        
+        button.setImage(UIImage(named: "start"), for: .normal)
+        button.addTarget(self, action: #selector(tapStartButton), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var stopButton:UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.frame = CGRect(x: ScreenWidth*0.5,y: ScreenHeight*0.7, width: ScreenWidth*0.5, height: ScreenHeight*0.3)
+        
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 20
+        
+        button.setImage(UIImage(named: "pause"),for: .normal)
+        button.addTarget(self, action: #selector(tapPauseButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func tapStartButton() {
+        
+    }
+    
+    @objc func tapPauseButton() {
+        
+    }
+    
+    @objc func tapResetButton() {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.view.addSubview(resetButton)
+        self.view.addSubview(startButton)
+        self.view.addSubview(stopButton)
     }
 
 
