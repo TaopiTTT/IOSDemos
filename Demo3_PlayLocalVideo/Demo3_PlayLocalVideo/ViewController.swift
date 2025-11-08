26aabab8626798e69fe6383df5dcc84158e03a5f
TaopiTTT <taopittt@qq.com>
2025-11-08 16:47:20 +0800

    init Demo3

---
+++ Demo3_PlayLocalVideo/Demo3_PlayLocalVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo3_PlayLocalVideo
+//
+//  Created by Tao on 2025/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
