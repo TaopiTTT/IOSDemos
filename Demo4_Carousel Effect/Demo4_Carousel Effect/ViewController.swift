367edaef055241a25be3bdc6d47593b2e87eba65
TaopiTTT <taopittt@qq.com>
2025-11-09 14:40:30 +0800

    init Demo4

---
+++ Demo4_Carousel Effect/Demo4_Carousel Effect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo4_Carousel Effect
+//
+//  Created by Tao on 2025/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
