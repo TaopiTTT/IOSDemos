2521c4533846e4121daf36fc149e5124dfaa2d00
TaopiTTT <taopittt@qq.com>
2025-11-11 20:22:19 +0800

    init Demo9

---
+++ Demo9_VideoBackground/Demo9_VideoBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo9_VideoBackground
+//
+//  Created by Tao on 2025/11/11.
+//
+
+import UIKit
+
+let frame = UIScreen.main.bounds
+let screenWidth = frame.width
+let screenHeight = frame.height
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
