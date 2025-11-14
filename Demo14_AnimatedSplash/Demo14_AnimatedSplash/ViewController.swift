8a5e8fae8a06aa1a6c162da41191883b6e78ea3b
TaopiTTT <taopittt@qq.com>
2025-11-14 22:22:44 +0800

    完成Demo14

--- Demo14_AnimatedSplash/Demo14_AnimatedSplash/ViewController.swift
+++ Demo14_AnimatedSplash/Demo14_AnimatedSplash/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
+let frame = UIScreen.main.bounds
+
 class ViewController: UIViewController {
 
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: frame)
+        let image = UIImage(named: "screen")
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    lazy var mask: CALayer = {
+        let layer = CALayer()
+        layer.contents = UIImage(named: "twitter")?.cgImage
+        layer.contentsGravity = .resizeAspect
+        layer.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
+        layer.position = CGPoint(x: self.imageView.frame.size.width/2, y: self.imageView.frame.height/2)
+        return layer
+    }()
+    
+    // 在短时间内放大，实现开屏动画
+    func animateMask() {
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 0.5
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut), CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)]
+        let initalBounds = NSValue(cgRect: self.mask.bounds)
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 90*0.9, height: 73*0.9))
+        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
+        keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        self.mask.add(keyFrameAnimation, forKey: "bounds")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(imageView)
+        imageView.layer.mask = self.mask
+        animateMask()
     }
-
 
 }
 
+extension ViewController: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        self.imageView.layer.mask = nil
+    }
+}
