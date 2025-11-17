e54dc081bd393a92baa5127774e43260f5ec579b
TaopiTTT <taopittt@qq.com>
2025-11-17 19:31:07 +0800

    initDemo15

---
+++ Demo15_SlideMenu/Demo15_SlideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo15_SlideMenu
+//
+//  Created by Tao on 2025/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
