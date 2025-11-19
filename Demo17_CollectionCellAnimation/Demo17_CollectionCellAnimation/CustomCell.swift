9a4ace727efdf4b3d4c8ac3109dca637452f6c9c
TaopiTTT <taopittt@qq.com>
2025-11-19 20:54:26 +0800

    完成Demo17

--- Demo17_CollectionCellAnimation/Demo17_CollectionCellAnimation/CustomCell.swift
+++ Demo17_CollectionCellAnimation/Demo17_CollectionCellAnimation/CustomCell.swift
@@ -4,11 +4,27 @@
 //
 //  Created by Tao on 2025/11/19.
 //
-
 import UIKit
 
 class CustomCell: UICollectionViewCell {
+    lazy var imageView: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: ScreenWidth - 60, height: 400))
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addSubview(imageView)
+//        backgroundColor = UIColor(red: 240 / 255, green: 248 / 255, blue: 1.0, alpha: 1.0)
+        layer.cornerRadius = 15
+        layer.masksToBounds = true
+        
+//        layer.shadowColor = UIColor.black.cgColor
+//        layer.shadowOpacity = 0.4
+//        layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+    }
 }
 
 
--- Demo17_CollectionCellAnimation/Demo17_CollectionCellAnimation/ViewController.swift
+++ Demo17_CollectionCellAnimation/Demo17_CollectionCellAnimation/ViewController.swift
@@ -7,13 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let Frame = UIScreen.main.bounds
+let ScreenWidth = Frame.width
+let ScreenHeight = Frame.height
 
+class ViewController: UIViewController {
+    
+    let id = "reusedCell"
+    
+    var selectedCell: CustomCell?
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: ScreenWidth-60, height: 400)
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: ScreenWidth, height: ScreenHeight), collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(CustomCell.classForCoder(), forCellWithReuseIdentifier: id)
+        return collectionView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Card Animation"
+        view.addSubview(collectionView)
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.delegate = self
     }
-
 
 }
 
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id, for: indexPath) as! CustomCell
+        switch indexPath.row {
+        case 0:
+            cell.imageView.image = UIImage(named: "1")
+        case 1:
+            cell.imageView.image = UIImage(named: "2")
+        case 2:
+            cell.imageView.image = UIImage(named: "3")
+        case 3:
+            cell.imageView.image = UIImage(named: "4")
+        default:
+            cell.imageView.image = UIImage(named: "5")
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedCell = collectionView.cellForItem(at: indexPath) as? CustomCell
+        let detailVC = DetailViewController(image: (selectedCell?.imageView.image)!)
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 40
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == UINavigationController.Operation.push {
+            return PushAnimationController()
+        } else {
+            return PopAnimationController()
+        }
+    }
+
+}
