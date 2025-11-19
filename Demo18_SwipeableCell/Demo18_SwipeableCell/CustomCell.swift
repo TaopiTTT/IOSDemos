e32461e8f9a28d1d3933c3e4d0f7841efb4fca53
TaopiTTT <taopittt@qq.com>
2025-11-19 21:08:43 +0800

    完成Demo18

---
+++ Demo18_SwipeableCell/Demo18_SwipeableCell/CustomCell.swift
@@ -0,0 +1,45 @@
+//
+//  CustomCell.swift
+//  Demo18_SwipeableCell
+//
+//  Created by Tao on 2025/11/19.
+//
+
+
+import UIKit
+
+struct Pattern {
+    let image: String
+    let name: String
+}
+
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addSubview(leftImageView)
+        addSubview(rightLabel)
+    }
+    
+    lazy var leftImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: ScreenWidth * 0.45, height: 60))
+        return imageView
+    }()
+
+    lazy var rightLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: ScreenWidth * 0.45, y: 0, width: ScreenWidth * 0.55, height: 60))
+        return label
+    }()
+}
